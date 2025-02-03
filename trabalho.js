1610cef7a2a54ee87a90786ae64d5fb718a6c9a3
Fernanda <101828371+FPetiz@users.noreply.github.com>
2025-02-03 12:38:37 -0300

    Final

--- trabalho.js
+++ trabalho.js
@@ -939,10 +939,10 @@ async function main() {
             objeto.position[1]*0.1,
             objeto.position[2]*0.1
           );
-
-          matrix = m4.multiply(matrix, m4.xRotation(Math.PI * objeto.rotation[0]));
+          
+          matrix = m4.multiply(matrix, m4.zRotation(Math.PI * objeto.rotation[2]));
           matrix = m4.multiply(matrix, m4.yRotation(Math.PI * objeto.rotation[1]));
-          matrix = m4.multiply(matrix, m4.zRotation(Math.PI * objeto.rotation[2]));     
+          matrix = m4.multiply(matrix, m4.xRotation(Math.PI * objeto.rotation[0]));            
           
           if (objeto.scale !== 1) {
             matrix = m4.scale(matrix, objeto.scale, objeto.scale, objeto.scale);
