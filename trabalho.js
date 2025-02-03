83296e3b6f9303b8250340555af62295e9c67d0a
Fernanda <101828371+FPetiz@users.noreply.github.com>
2025-02-03 13:00:35 -0300

    Teste mudança do cálculo das rotações

--- trabalho.js
+++ trabalho.js
@@ -940,9 +940,15 @@ async function main() {
             objeto.position[2]*0.1
           );
           
-          matrix = m4.multiply(matrix, m4.yRotation(Math.PI * objeto.rotation[1]));
-          matrix = m4.multiply(matrix, m4.zRotation(Math.PI * objeto.rotation[2]));
-          matrix = m4.multiply(matrix, m4.xRotation(Math.PI * objeto.rotation[0]));            
+          // Aplica a rotação em espaço local
+          const xRotation = m4.xRotation(Math.PI * objeto.rotation[0]);
+          const yRotation = m4.yRotation(Math.PI * objeto.rotation[1]);
+          const zRotation = m4.zRotation(Math.PI * objeto.rotation[2]);
+          
+          // Combina as rotações
+          matrix = m4.multiply(matrix, zRotation); 
+          matrix = m4.multiply(matrix, yRotation); 
+          matrix = m4.multiply(matrix, xRotation);             
           
           if (objeto.scale !== 1) {
             matrix = m4.scale(matrix, objeto.scale, objeto.scale, objeto.scale);
